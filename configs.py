ec823279626e963fa2c6c9f12cf422775d24577f
bv738 <144891499+bv738@users.noreply.github.com>
2023-10-15 11:12:44 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,11 +8,11 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "20389440"))
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6413115065:AAGAIXWmI00Zf_-7GVKASEQL0B5BIaWMJCU")
+    FSUB = getenv("FSUB", "IrisMoviesCh")
+    CHID = int(getenv("CHID", "-1001747312807"))
+    SUDO = list(map(int, getenv("SUDO", "730412993").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://bvv:gRXsDeLnAdE71DPu@cluster0.2on6kqk.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
 
